4fe80a09cb7641849c9c56c825ced8dc3f95647d
Eliseu dos Santos da Silva <s.eliseu@proton.me>
2025-08-06 07:09:41 -0300

    Update installSpecifyingVersionSemantic.task.js

--- src/installSpecifyingVersionSemantic.task.js
+++ src/installSpecifyingVersionSemantic.task.js
@@ -12,8 +12,9 @@ module.exports = async () => {
   const versionSuffix = semantic_version
     ? `@${semantic_version}`
     : '';
+  const silentFlag = process.env.RUNNER_DEBUG === '1' ? '' : '--silent';
 
-  const {stdout, stderr} = await exec(`npm install semantic-release${versionSuffix} --no-audit`, {
+  const {stdout, stderr} = await exec(`npm install semantic-release${versionSuffix} --no-audit ${silentFlag}`, {
     cwd: path.resolve(__dirname, '..')
   });
   core.debug(stdout);
